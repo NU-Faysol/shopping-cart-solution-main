d185a1a93473ab8d8bdfbee94c3a2132cae4714b
NU-Faysol <nizamfaysol16@gmail.com>
2022-02-18 18:08:21 +0600

    first commit

---
+++ withoutFunction.js
@@ -0,0 +1,62 @@
+                    
+//                     //for phone plus minus
+//     document.getElementById('phone-plus').addEventListener('click', function(){
+
+//         const caseInput = document.getElementById('phone-number');
+                    
+//             let caseNumber = caseInput.value;
+//                 caseNumber = parseInt(caseNumber) + 1;
+//                 caseInput.value = caseNumber;
+                    
+//             const caseTotal = document.getElementById('phone-total');
+//                   caseTotal.innerText = caseNumber * 1219;
+                    
+                    
+//         });
+                    
+//     document.getElementById('phone-minus').addEventListener('click', function(){
+                    
+//         const caseInput = document.getElementById('phone-number');
+                    
+//             let caseNumber = caseInput.value;
+                        
+//                 if(caseNumber > 0){
+//                         caseNumber = parseInt(caseNumber) - 1;
+//                     }
+//                     caseInput.value = caseNumber;
+                            
+//         const caseTotal = document.getElementById('phone-total');
+//               caseTotal.innerText = caseNumber * 1219;
+
+//         });                   
+                    
+                    
+//                     //for case plus-minus
+// document.getElementById('case-plus').addEventListener('click', function(){
+
+//     const caseInput = document.getElementById('case-number');
+
+//     let caseNumber = caseInput.value;
+//           caseNumber = parseInt(caseNumber) + 1;
+//           caseInput.value = caseNumber;
+
+//     const caseTotal = document.getElementById('case-total');
+//           caseTotal.innerText = caseNumber * 59;
+
+
+// });
+
+// document.getElementById('case-minus').addEventListener('click', function(){
+
+//     const caseInput = document.getElementById('case-number');
+
+//     let caseNumber = caseInput.value;
+    
+//         if(caseNumber > 0){
+//             caseNumber = parseInt(caseNumber) - 1;
+//         }
+//         caseInput.value = caseNumber;
+        
+//     const caseTotal = document.getElementById('case-total');
+//           caseTotal.innerText = caseNumber * 59;
+// });
